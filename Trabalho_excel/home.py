133c448a91aeec810173f6a1733dd2193ca253c7
Fábio Eduardo Circuncisão <32108788+caffranksereia@users.noreply.github.com>
2022-06-09 21:19:24 -0300

    Update home.py

--- Trabalho_excel/home.py
+++ Trabalho_excel/home.py
@@ -1,82 +1,64 @@
-# Utilizar pip install selenium
-# Utilizar pip install webdriver-manager
-# Utilizar pip install openpyxl
-# Utilizar pip install pandas
-
-import os            #Criar pasta destino
-import time          #Coloca tempo de espera
-import shutil        #Movimentação de arquivo
-import zipfile       #Descompactação de arquivo
-import pandas as pd  #Trabalhar com arquivos excel
-
-#Envio de email com anexo
+import os
+import time
+import shutil
+import zipfile
+import pandas as pd
 import smtplib
 from email import encoders
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-
-#Automatização do download de arquivo utilizando Chrome
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-#Solicita crendencial de acesso
+
 user = input("Informe o login de acesso:")
 password_user = input("Informe a senha de acesso:")
 
-driver = webdriver.Chrome(ChromeDriverManager().install()) #Inicia o Chrome
-driver.maximize_window() #Maximiza a janela do Chrome
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
 
-# O time sleep no código, aguarda o carregamento antes de executar o próximo, assim dando tempo de carregar a pagina por completo.
-
-#Acessa o site da faculdade e clica em "Entrar"
 driver.get("https://estudante.estacio.br/")
 time.sleep(5)
 
-#Clica no botão Entrar para começar o login
-button0 = driver.find_element_by_xpath("/html/body/div[1]/section/div/div/div/section/div[1]/button").click()
-time.sleep(5)
+bt0 = driver.find_element_by_xpath("/html/body/div[1]/section/div/div/div/section/div[1]/button").click()
+time.sleep(15)
 
-#Pega o campo do login pelo ID e envia o email de acesso.
 username_textbox=driver.find_element_by_id("i0116")
 username_textbox.send_keys(user)
 
-#Clica no botão para avançar pelo ID
-button1 = driver.find_element_by_id("idSIButton9").click()
-time.sleep(5)
+bt1 = driver.find_element_by_id("idSIButton9").click()
+time.sleep(10)
 
-#Pega o campo da senha pelo ID e envia a senha de acesso.
 password=driver.find_element_by_id("i0118")
 password.send_keys(password_user)
 
-#Clica para efetuar o login no site
+
 login_attempt = driver.find_element_by_id("idSIButton9")
 login_attempt.submit()
-time.sleep(5)
+time.sleep(10)
 
-#Clica em "Não" na caixa de mensagem que pergunta se quer se manter conectado.
-button2 = driver.find_element_by_id("idBtn_Back").click()
-time.sleep(5)
+bt2 = driver.find_element_by_id("idBtn_Back").click()
+time.sleep(10)
 
-#Acessa a diciplina de Python
-button3 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/div/section[2]/div/div/div/article/div/div[1]/section/header/button").click()
-time.sleep(5)
 
-#Acessa o tema do trabalho
-button4 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[1]/section/div/div/article/section[3]/div[2]/div[3]/div/section/div[5]/a").click()
-time.sleep(5)
+bt3 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/div/section[2]/div/div/div/article/div/div[1]/section/header/button").click()
+time.sleep(10)
 
-#Clica no conteudo complementar para efetuar o download do arquivo.
-button5 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[2]/section/div/div/article/section/section/section/div[3]/div[2]/div/section/div/div[1]/button").click()
-time.sleep(5)
 
-#Fecha o Browser Google Chrome
+bt4 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[1]/section/div/div/article/section[3]/div[2]/div[3]/div/section/div[5]/a").click()
+time.sleep(10)
+
+bt5 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[2]/section/div/div/article/section/section/section/div[3]/div[2]/div/section/div/div[1]/button").click()
+time.sleep(10)
+
+
 driver.quit()
 
-#Cria o diretório onde iremos salvar o arquivo baixado e verifica se a pasta já existe.
+
 os.makedirs("./TrabalhoAltamira", exist_ok=True)
 
-#Pega o arquivo baixado e move para o diretorio que criado.
+
 source = r"C:/Users/201801196982/Downloads/combate_pirataria.zip"
 destination = r"C:/Users/201801196982/PycharmProjects/pythonProject/combate_pirataria.zip"
 shutil.move(source, destination)
@@ -139,11 +121,9 @@ df = df.append(dfmedia,ignore_index=True)
 df = df.append(dfmin,ignore_index=True)
 df = df.append(dfmax,ignore_index=True)
 
-df.to_excel(r"C:/Fabio/TrabalhoAltamira/Resultado_Tabela.xlsx", index=False, header=True)
+df.to_excel(r"C:/Users/201801196982/PycharmProjects/pythonProject/ResTabela.xlsx", index=False, header=True)
 
-#Envio de e-mail automatico utilizando outlook
 
-#Login e senha, e autenticação com o servidor
 host = "smtp.office365.com"
 port = 587
 user_email = input("Informe o e-mail de acesso:")
@@ -155,16 +135,13 @@ server.ehlo()
 server.starttls()
 server.login(user_email, password_email)
 
-#Informa o destinatario, titulo, monta a mensagem do corpo(HTML) e anexo o arquivo.
+
 email_msg = MIMEMultipart()
 email_msg["From"] = user_email
-email_msg["to"] = "fabioeduardocircuncisao@gmail.com"
+email_msg["to"] = "altamira.queiroz@estacio.br"
 email_msg["Subject"] = "Trabalho - Paradigmas de Linguagens de Programação em Python"
 
-menssage = """<p>Olá, boa noite! Tudo bem?</p>
-<p>
-<p>Segue o arquivo em anexo conforme solicitado.</p>
-</p>
+menssage = """<p>Segue em anexo</p>
 <p>Atenciosamente,</p>
 <p>Fabio Eduardo</p> &
 <p>João Vitor </p>
@@ -173,7 +150,7 @@ menssage = """<p>Olá, boa noite! Tudo bem?</p>
 """
 email_msg.attach(MIMEText(menssage, "html"))
 
-anexo_arq = "C:/Fabio/TrabalhoAltamira/Resultado_Tabela.xlsx"
+anexo_arq = "C:/Users/201801196982/PycharmProjects/pythonProject/ResTabela.xlsx"
 attachment = open(anexo_arq, "rb")
 
 att = MIMEBase("application","octet-stream")
